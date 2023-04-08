568765c91e487393b4b24a04440acf2010c6995c
Lyb604cat <1178885046@qq.com>
2023-04-08 09:33:47 +0800

    UICharacterView

---
+++ Src/Client/Assets/Scripts/UI/UICharaterView.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICharaterView : MonoBehaviour
+{
+    public GameObject[] charaters;
+
+    private int currentCharacter = 0;
+    public int CurrentCharacter
+    {
+        get
+        {
+            return currentCharacter;
+        }
+        set
+        {
+            currentCharacter = value;
+            this.UpdateCharacter();
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public virtual void UpdateCharacter()
+    {
+        for (int i = 0; i < charaters.Length; i++)
+        {
+            charaters[i].SetActive(i == this.currentCharacter);
+        }
+    }
+
+}
