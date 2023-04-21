4ec606b25a4bafb793dfb3eefe2999b0f2b5f257
LYB <1178885046@qq.com>
2023-04-21 17:32:22 +0800

    04211732

--- Src/Client/Assets/Scripts/GameObject/EntityController.cs
+++ Src/Client/Assets/Scripts/GameObject/EntityController.cs
@@ -1,18 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Entities;
+using SkillBridge.Message;
 using UnityEngine;
 
+/// <summary>
+/// 根据数据驱动，控制角色行为
+/// </summary>
 public class EntityController : MonoBehaviour
 {
+    public Animator anim;
+    public Rigidbody rb;
+    private AnimatorStateInfo currentBaseState;
+
+    public Entity entity;
+
+    public Vector3 position;//位置
+    public Vector3 direction;//方向
+    Quaternion lastRotation;
+
+    public float speed;
+    public float animSpeed = 1.5f;
+    public float jumpPower = 3.0f;
+
+    public bool isPlayer = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(entity != null)
+        {
+            this.UpdateTransform();
+        }
+        if(! this.isPlayer )
+        {
+            rb.useGravity = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void UpdateTransform()
     {
-        
+        this.position = GameObjectTool.LogicToWorld(entity.position);
+    }
+
+    void OnDestroy()
+    {
+        if( entity != null )
+        {
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (this.entity == null)
+            return;
+    }
+
+    public void OnEntityEvent(EntityEvent entityEvent)
+    {
+        switch (entityEvent)
+        {
+            case EntityEvent.Idle:
+
+                break;
+            case EntityEvent.None:
+
+                break;
+        }
     }
 }
--- Src/Client/Assets/Scripts/GameObject/PlayerInputController.cs
+++ Src/Client/Assets/Scripts/GameObject/PlayerInputController.cs
@@ -6,18 +6,21 @@ using UnityEngine.PlayerLoop;
 using Entities;
 using SkillBridge.Message;
 
+/// <summary>
+/// 将玩家输入转换为实体行为，比如说应该做什么、怎么做
+/// </summary>
 public class PlayerInputController : MonoBehaviour
 {
-    //public Rigidbody rb;
-    //SkillBridge.Message.CharacterState state;
+    public Rigidbody rb;
+    SkillBridge.Message.CharacterState state;
 
     public Character character;
 
-    //public float rotateSpeed = 2.0f;
+    public float rotateSpeed = 2.0f;
 
-    //public float turnAngle = 10;
+    public float turnAngle = 10;
 
-    //public int speed;
+    public int speed;
 
     public EntityController entityController;
 
@@ -27,25 +30,101 @@ public class PlayerInputController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //state
+        state = SkillBridge.Message.CharacterState.Idle;
+        if (this.character == null)
+        {
+            DataManager.Instance.Load();
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    /// <summary>
+    /// 检测键盘输入，作出响应响应，send响应结果
+    /// </summary>
     void FixedUpdate()
     {
         if(character == null)
-        {
             return;
-        }
-        float v = Input.GetAxis("Vertical");
-        if(v > 0)
-        {
 
+
+        float v = Input.GetAxis("Vertical");
+        if(v > 0.01)
+        {
+            if(state != SkillBridge.Message.CharacterState.Move)
+            {
+                state = SkillBridge.Message.CharacterState.Move;
+                this.character.MoveForward();
+                this.SendEntityEvent(EntityEvent.MoveFwd);
+            }
+            this.rb.velocity = this.rb.velocity.y * Vector3.up + GameObjectTool.LogicToWorld(character.direction) * (this.character.speed + 9.18f) / 100f;
+        }else if (v < -0.01)
+        {
+            if (state != SkillBridge.Message.CharacterState.Move)
+            {
+                state = SkillBridge.Message.CharacterState.Move;
+                this.character.MoveForward();
+                this.SendEntityEvent(EntityEvent.MoveFwd);
+            }
+            this.rb.velocity = this.rb.velocity.y * Vector3.up + GameObjectTool.LogicToWorld(character.direction) * (this.character.speed + 9.18f) / 100f;
+        }
+        else
+        {
+            if(state != SkillBridge.Message.CharacterState.Idle)
+            {
+                state = CharacterState.Idle;
+                this.rb.velocity = Vector3.zero;
+                this.character.Stop();
+                this.SendEntityEvent(EntityEvent.Idle);
+            }
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            this.SendEntityEvent(EntityEvent.Jump);
+        }
+
+        float h = Input.GetAxis("Horizontal");
+        if(h < -0.1 || h > 0.1)
+        {
+            this.transform.Rotate(0, h * rotateSpeed, 0);
+            Vector3 dir = GameObjectTool.LogicToWorld(character.direction);
+            Quaternion rot = new Quaternion();
+            rot.SetFromToRotation(dir, this.transform.forward);
+
+            if (rot.eulerAngles.y > this.turnAngle && rot.eulerAngles.y < (360 - this.turnAngle))
+            {
+                character.SetDirection(GameObjectTool.WorldToLogic(this.transform.forward));
+                rb.transform.forward = this.transform.forward;
+                this.SendEntityEvent(EntityEvent.None);
+            }
+        }
+    }
+
+    Vector3 lastPos;
+    float lastSync = 0;
+    /// <summary>
+    /// 位置同步
+    /// </summary>
+    private void LateUpdate()
+    {
+        Vector3 offset = this.rb.transform.position - lastPos;
+        this.speed = (int)(offset.magnitude * 100f / Time.deltaTime);
+        this.lastPos = this.rb.transform.position;
+
+        if((GameObjectTool.WorldToLogic(this.rb.transform.position) - this.character.position).magnitude > 50)
+        {
+            this.character.SetPosition((GameObjectTool.WorldToLogic(this.rb.transform.position)));
+            this.SendEntityEvent(EntityEvent.None);
+        }
+        this.transform.position = this.rb.transform.position; 
+    }
+
+
+    void SendEntityEvent(EntityEvent entityEvent)
+    {
+        if(entityController != null)
+        {
+            entityController.OnEntityEvent(entityEvent);
         }
     }
 }
